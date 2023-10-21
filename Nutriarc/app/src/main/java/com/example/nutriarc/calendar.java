178a6f048b2428d80d1cbf393701e5812c209395
Rachel Loren <114191692+GumdropHorse@users.noreply.github.com>
2023-10-21 15:23:47 -0400

    the first skeleton of the project
    This commit is just uploading some of the beginning front-end layout of the project.

---
+++ Nutriarc/app/src/main/java/com/example/nutriarc/calendar.java
@@ -0,0 +1,26 @@
+package com.example.nutriarc;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.nutriarc.databinding.ActivityCalendarBinding;
+
+public class calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
---
+++ Nutriarc/app/src/main/java/com/example/nutriarc/recipies.java
@@ -0,0 +1,14 @@
+package com.example.nutriarc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recipies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipies);
+    }
+}
